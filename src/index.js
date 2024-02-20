595b920e9c6a572f6ef8e5ee1cec6c1c8c03aa8e
Freddie Palfreman <freddiepalfreman@hotmail.com>
2024-02-20 15:46:45 +0000

    Reorganise entry point

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
-import SearchBar from './components/SearchBar';
-import BusinessList from './components/Business';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <SearchBar />
-    <BusinessList />
-  </React.StrictMode>
-);
+root.render(<StrictMode><App /></StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
